78f28d714344f23bc40064f782508e27c61553d3
nonuno <126319565+nnnnev@users.noreply.github.com>
2023-06-01 04:00:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,3 @@
-// 参考例
-
-// let fruits = "りんご";
-// console.log(fruits);
-
-// let username = "ささきももこ";
-// console.log(username);
-
-// 練習問題
-
-// let myname = "佐々木もも子";
-// let company = "NEV";
-// let favorite = "歌うこと";
-// let favoriteNumber = "2";
-
-// console.log("みなさんこんにちは！！！！！！！\n私は"
-// + myname + "といいます。\n所属は"
-// + company + "です。\n好きなことは"
-// + favorite + "です。\n好きな数字は"
-// + favoriteNumber + "です。\nよろしくお願いします！！！！");
-
-
-// 練習問題
-
-// let point = 69;
-
-// 80点以上なら合格
-// 70点以上ならもうちょっと(70～79)
-// それ以外は不合格
-// …をコンソールに出力
-
-// if (point >= 80) {
-//     console.log("合格");
-// } else if (point < 70) {
-//     console.log("不合格");
-// } else {
-//     console.log("もうちょっと");
-// }
-
-
 
 // 202305FE講習課題・fizzbuzz問題
 
@@ -51,7 +11,5 @@ for (let count = 1; count <= 100; count++) {
 
     } else {
         console.log(count);
-        console.log(count);
-
     }
 }
