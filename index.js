acdd26ac44a4244c10bda5e93e038ddd6ddca6a8
nonuno <126319565+nnnnev@users.noreply.github.com>
2023-06-01 04:10:33 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// 202305FE講習課題・fizzbuzz問題
+
+for (let count = 1; count <= 100; count++) {
+    if (count % 3 === 0) {
+        console.log('fizz');
+    } else if (count % 5 === 0) {
+        console.log('buzz');
+    }   else if (count % 15 === 0) {
+            console.log('fizzbuzz');
+
+    } else {
+        console.log(count);
+
+    }
+}
